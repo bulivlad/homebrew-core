9a81c3c53f0030e4f80fdedae70417d75de07b7c
Vlad Bulimac <vbulimac@deloittece.com>
2019-12-19 18:34:14 +0200

    Adds gradle 5.6.3

---
+++ Formula/gradle@563.rb
@@ -0,0 +1,20 @@
+class GradleAT563 < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-5.6.3-all.zip"
+  sha256 "342f8e75a8879fa9192163fa8d932b9f6383ea00c1918a478f0f51e11e004b60"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin docs lib samples src]
+    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+  end
+end
