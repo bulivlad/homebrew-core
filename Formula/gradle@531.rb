801c452cb839dba617d2ea695f69a6a238aa8ba9
Vlad Bulimac <vbulimac@deloittece.com>
2020-10-19 14:33:20 +0300

    Adds gradle v5.3.1

---
+++ Formula/gradle@531.rb
@@ -0,0 +1,20 @@
+class GradleAT531 < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-5.3.1-all.zip"
+  sha256 "b018a7308cb43633662363d100c14a3c41c66fd4e32b59e1dfc644d6fd2109f6"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin docs lib samples src]
+    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+  end
+end
